04ac51764f510e7562a9da82abcc732afbdd25f3
Azusa404 <fengchui233@gmail.com>
2018-04-27 23:53:20 +0900

    第一次推送
    总之先把写了的推送了。目前有的功能是移动，跳跃（检测防止二次跳跃），敌人（来回移动），死亡特效。人物素材用了unity2d酱

---
+++ Assets/_Script/HazardScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HazardScript : MonoBehaviour {
+
+    public float w;
+    public float speed;
+    void Start()
+    {
+
+    }
+	
+
+	void Update () {
+        
+        // transform.position = new Vector3(Mathf.PingPong(Time.time, 3), transform.position.y, transform.position.z);
+        transform.position = new Vector3(transform.position.x + w*Mathf.Sin(Time.time*speed), transform.position.y, transform.position.z);
+	}
+}
---
+++ Assets/_Script/catController.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class catController : MonoBehaviour {
+
+    public Rigidbody2D cat2d;
+    public float movespeed;
+    public float jumpheight;
+    public Transform groundcheck;
+    public LayerMask whatisground;
+    public float groundcheckRadius;
+    public bool facingrRight;
+    public Transform startPoint;
+    public GameObject Explode;
+
+
+    private bool onGround;
+    private Vector3 theScale;
+    private float h;
+    
+    void Start()
+    {
+        cat2d = GetComponent<Rigidbody2D>();
+        facingrRight = true;
+        cat2d.transform.position = startPoint.position;
+    }
+
+    void FixedUpdate()
+    {
+        ////检测键盘输入（两种方法）
+        //左
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            cat2d.velocity = new Vector2(-movespeed, cat2d.velocity.y);
+
+        }
+        //右
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            cat2d.velocity = new Vector2(movespeed, cat2d.velocity.y);
+
+        }
+        //float moveHorizontal = Input.GetAxis("Horizontal");
+        //float moveVertical = Input.GetAxis("Vertical");
+        //Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        //cat2d.AddForce(movement * movespeed);
+
+        //检测是否在跳跃
+        onGround = Physics2D.OverlapCircle(groundcheck.position, groundcheckRadius, whatisground);
+
+        //跳跃
+        if (Input.GetKey(KeyCode.Space) && onGround)
+        {
+            cat2d.velocity = new Vector2(cat2d.velocity.x, jumpheight);
+        }
+        //判断面向是否正确，不正确则翻转
+        h = cat2d.velocity.x;
+        if (h > 0 && !facingrRight)
+        {
+            Flip();
+        }
+        if (h < 0 && facingrRight)
+        {
+            Flip();
+        }
+
+    }
+
+    void Flip()
+    {   //翻转x轴scale
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+        //翻转面向标记
+        facingrRight = !facingrRight;
+        //Debug.Log("Flip");
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("1234"))
+        {
+            Death();
+            Reborn();
+        }
+    }
+
+    void Death()
+    {
+        Instantiate(Explode, transform.position, transform.rotation);
+        enabled = false;
+        GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+        GetComponent<Renderer>().enabled = false;
+        Debug.Log("You've been Dead");
+    }
+
+    void Reborn()
+    {
+        transform.position = startPoint.position;
+        enabled = true;
+        GetComponent<Renderer>().enabled = true;
+    }
+}
+  
